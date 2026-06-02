aa434913f5cb363e2a514d7f3e3631fe82ca9f5b
gokarna28 <gokarnachy28@gmail.com>
2026-06-02 13:42:45 +0545

    fix: right and bottomn

--- includes/class-hb-postcard.php
+++ includes/class-hb-postcard.php
@@ -447,7 +447,7 @@ class Hb_Postcard {
 		$ref = apply_filters(
 			'hb_postcard_qr_placement_ref',
 			array(
-				'x'    => 1282,
+				'x'    => 1300,
 				'y'    => 780,
 				'size' => 158,
 			),
